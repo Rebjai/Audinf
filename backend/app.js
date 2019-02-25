a6bc2d31e5299628eafe0fbfa20a07dea37b0514
qqavon <qqavon@gmail.com>
2019-02-25 23:15:15 +0100

    init commit, prepared files

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+    , bodyParser = require('body-parser')
+    , cors = require('cors')
+    , mongoose = require('mongoose')
+    , error = require('./middlewares/error')
+    , { DB_NAME, USER_NAME, PASSWORD, CLUSTER } = require('./constants.json')
+
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+mongoose.connect(`mongodb+srv://${USER_NAME}:${PASSWORD}@${CLUSTER}.mongodb.net/${DB_NAME}`, {
+    useNewUrlParser: true
+})
+
+app.use('/api', require('./routes/index'))
+app.use(error.notFound)
+app.use(error.catchError)
+
+module.exports = app
---
+++ backend/middlewares/error.js
@@ -0,0 +1,20 @@
+exports.notFound = (req, res, next) => {
+    const err = new Error('Not found')
+    err.status = 404
+
+    next(err)
+}
+
+exports.catchAsync = (fn) => {
+    return (req, res, next) => {
+        fn(req, res, next).catch(err => next(err))
+    }
+}
+
+exports.catchError = (err, req, res, next) => {
+    err.status = err.status || 500
+
+    return res.status(err.status).json({
+        message: err.message
+    })
+}
