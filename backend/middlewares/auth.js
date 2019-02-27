41903fc39feb272ff1f185b557e9943616f00d5a
qqavon <qqavon@gmail.com>
2019-02-27 19:02:22 +0100

    Added auth middleware

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,18 @@
+const { check } = require('express-validator/check')
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET } = require('../constants.json')
+
+exports.verifyToken = (req, res, next) => {
+    
+    try {
+        const decoded = jwt.verify(req.headers.authorization, JWT_SECRET)
+        req.userData = decoded
+    }
+    catch(err) {
+        return res.status(400).json({
+            message: "Błąd z sesją."
+        })
+    }
+
+    next()
+}
