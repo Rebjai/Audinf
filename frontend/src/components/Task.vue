9e4bce54154625eb2cf973d298602d1fa8587028
qqavon <qqavon@gmail.com>
2019-03-01 23:47:35 +0100

    Added basic task component

---
+++ frontend/src/components/Task.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="task-item">
+        <span> {{ task.name }} </span>
+        <button>X</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['task']
+}
+</script>
+
+<style>
+    .task-item {
+        display: grid;
+        grid-template-columns: 1fr 50px;
+        align-items: center;
+    }
+    .task-item > button {
+        border: solid #e7e7e7 1px;
+        border-radius: 5px;
+        background-color: transparent;
+        width: 25px;
+        height: 25px;
+        justify-self: center;
+    }
+</style>
